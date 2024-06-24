fe04aafca511cf953d95c25b6d05284724669dc6
vinícius matos <viniciusmatos.tmjr@gmail.com>
2024-06-24 19:35:32 -0300

    configuração final padronizando MBIF e quais rodar

--- code/novos modelos e tratamentos de dados/lib/classifier.py
+++ code/novos modelos e tratamentos de dados/lib/classifier.py
@@ -1,144 +1,136 @@
 import numpy as np
 
+class Classifier:
+    def __init__(self,model,FeatureSelectModel=None):
+        self.model = model
+        self.feature_selection = False
+        self.FeatureSelectModel = FeatureSelectModel
 
-class NaiveBayesClassifier:
-    """
-    A Naive Bayes Classifier using Kernel Density Estimation with Parzen windows.
-
-    The classifier implements two kernels for parzen window  - Radial and Hypercube
-
-    It also implements Single bandwidth model and class-specific Multi bandwidth model
-
-    The kernel and model type are passed as arguments to class object initialization.
-
-    Along with the number of bandwidths necessary, in case of Multi bandwidth model.
-    """
-
-    def __init__(self, bandwidth=1, kernel='radial', multi_bw=False):
-        """
-        Initialize the classifier with proper parameters.
-
-        :param bandwidth: An integer giving the number of bandwidths necessary
-        :param kernel: A string specifying the kernel to be used for the model
-        :param multi_bw: A boolean variable specifying if the Multi bandwidth
-                        model is to be used.
-                        By default Single bandwidth model is selected.
-        """
-        self.priors = dict()
-        self.dim = 1
-        self.multi_bw = multi_bw
-        self.bandwidth = bandwidth
-        if kernel == "radial":
-            self.kernel = self.radial
-        elif kernel == "hypercube":
-            self.kernel = self.hypercube
-
-    def hypercube(self, k):
-        """
-        Hypercube kernel for Density Estimation.
-        """
-        return np.all(k < 0.5, axis=1)
-
-    def radial(self, k):
-        """
-        Radial Kernel for Density estimation.
-        """
-        const_part = (2 * np.pi) ** (-self.dim / 2)
-        return const_part * np.exp(-0.5 * np.add.reduce(k ** 2, axis=1))
-
-    def parzen_estimation(self, h, x, x_train):
-        """
-        Density estimation for a single sample against the training set with
-        parzen window using the specified bandwidth, kernel.
-
-        :param h: An integer value giving the bandwidth to be used for the class.
-        :param x: A single input sample, whose density needs to be estimated.
-        :param x_train: Array of input data to calculate KDE value against.
-        :return: A single float value giving the density of the function at the given point.
-        """
-        N = x_train.shape[0]
-        dim = self.dim
-        k = np.abs(x - x_train) * 1.0 / h
-        summation = np.add.reduce(self.kernel(k))
-        return summation / (N * (h ** dim))
-
-    def KDE(self, h, x_test, x_train):
-        """
-        Kernel Density Estimation based on the parameters set.
-
-        :param h: An integer value giving the bandwidth to be used for the class.
-        :param x_test: Array of input data to make predictions.
-        :param x_train: Array of input data to calculate KDE value against.
-        :return: A list of floats giving the density estimation values for each
-                 row in x_test, x_test[i] calculated against the training set, previously set
-        """
-        P_x = np.zeros(len(x_test))
-        N = x_train.shape[0]
-        dim = self.dim
-        for i in range(len(x_test)):
-            P_x[i] = self.parzen_estimation(h, x_test[i], x_train)
-
-        return P_x
-
-    def fit(self, X, Y):
-        """
-        Fits the model to the training set.
-        Since KDE is a lazy learner we just need to save the necessary information.
-
-        :param X: Array of input data
-        :param Y: Array of output labels
-        :return: None
-        """
-        self.x_train = X
-        self.y_train = Y
-        self.dim = X.shape[1]
-        labels = set(Y)
-        for c in labels:
-            self.priors[c] = float(len(Y[Y == c])) / len(Y)
-
-    def predict(self, x_test):
-        """
-        Predict the labels of testing set, using KDE.
-
-        :param x_test: Array of input data to make predictions.
-        :return: Predicted labels of the data.
-        """
-        N, D = x_test.shape
-        priors = self.priors
-        K = len(priors)
-        P = np.zeros((N, K))
-        x_train = self.x_train
-        y_train = self.y_train
-        if self.multi_bw:
-            bw = self.bandwidth
+    def predict(self,x_features):
+        if self.feature_selection:
+            x_features_selected = self.feature_selection.transform(x_features)
         else:
-            bw = np.repeat(self.bandwidth, K)
-        for c, p in priors.items():
-            P[:, int(c)] = self.KDE(bw[int(c)], x_test, x_train[y_train == c]) * p
+            x_features_selected = x_features
+        y_predicted = self.model.predict(x_features_selected)
+        return y_predicted
 
-        pred_y = np.argmax(P, axis=1)
-        self.pred_y = pred_y
+    def fit(self,x_features,y_train):
+        feature_selection = True
+        if feature_selection:
+            if self.FeatureSelectModel is None:
+                self.feature_selection = FeatureSelect()
+            else:
+                self.feature_selection = self.FeatureSelectModel
+            
+            x_train_features_selected = self.feature_selection.fit_transform(x_features,y_train)
+        else:
+            x_train_features_selected = x_features
+        self.model.fit(x_train_features_selected,y_train)
+        y_predicted = self.model.predict(x_train_features_selected)
+        return y_predicted
 
-        return pred_y
 
-    def accuracy(self, y_test):
-        """
-        Calculates the accuracy between the predicted label and actual labels.
+class FeatureSelect:
+    def __init__(self, n_features_select=4, n_csp_pairs=2):
+        self.n_features_select = n_features_select
+        self.n_csp_pairs = n_csp_pairs
+        self.features_selected_indices=[]
 
-        :param y_test: Array of actual output labels of Testing set.
-        :return: A float value giving the accuracy.
-        """
-        pred_y = self.pred_y
-        return np.array([pred_y == y_test]).mean()
+    def fit_transform(self,x_train_features,y_train):
+        MI_features = self.MIBIF(x_train_features, y_train)
+        MI_sorted_idx = np.argsort(MI_features)[::-1]
+        features_selected = MI_sorted_idx[:self.n_features_select]
 
-    def score(self, x_test, y_test):
-        """
-        Function that runs both Predict and Accuracy and returns the accuracy
-        score of the model.
+        paired_features_idx = self.select_CSP_pairs(features_selected, self.n_csp_pairs)
+        x_train_features_selected = x_train_features[:, paired_features_idx]
+        self.features_selected_indices = paired_features_idx
 
-        :param x_test: Array of input data to make predictions.
-        :param y_test: Array of actual output labels of Testing set.
-        :return: A float value giving the accuracy of the model.
-        """
-        self.predict(x_test)
-        return self.accuracy(y_test)
+        return x_train_features_selected
+
+    def transform(self,x_test_features):
+        return x_test_features[:,self.features_selected_indices]
+
+    def MIBIF(self, x_features, y_labels):
+        def get_prob_pw(x,d,i,h):
+            n_data = d.shape[0]
+            t=d[:,i]
+            kernel = lambda u: np.exp(-0.5*(u**2))/np.sqrt(2*np.pi)
+            prob_x = 1 / (n_data * h) * sum(kernel((np.ones((len(t)))*x- t)/h))
+            return prob_x
+
+        def get_pd_pw(d, i, x_trials):
+            n_data, n_dimensions = d.shape
+            if n_dimensions==1:
+                i=1
+            t = d[:,i]
+            min_x = np.min(t)
+            max_x = np.max(t)
+            n_trials = x_trials.shape[0]
+            std_t = np.std(t)
+            if std_t==0:
+                h=0.005
+            else:
+                h=(4./(3*n_data))**(0.2)*std_t
+            prob_x = np.zeros((n_trials))
+            for j in range(n_trials):
+                prob_x[j] = get_prob_pw(x_trials[j],d,i,h)
+            return prob_x, x_trials, h
+
+        y_classes = np.unique(y_labels)
+        n_classes = len(y_classes)
+        n_trials = len(y_labels)
+        prob_w = []
+        x_cls = {}
+        for i in range(n_classes):
+            cls = y_classes[i]
+            cls_indx = np.where(y_labels == cls)[0]
+            prob_w.append(len(cls_indx) / n_trials)
+            x_cls.update({i: x_features[cls_indx, :]})
+
+        prob_x_w = np.zeros((n_classes, n_trials, x_features.shape[1]))
+        prob_w_x = np.zeros((n_classes, n_trials, x_features.shape[1]))
+        h_w_x = np.zeros((x_features.shape[1]))
+        mutual_info = np.zeros((x_features.shape[1]))
+        parz_win_width = 1.0 / np.log2(n_trials)
+        h_w = -np.sum(prob_w * np.log2(prob_w))
+
+        for i in range(x_features.shape[1]):
+            h_w_x[i] = 0
+            for j in range(n_classes):
+                prob_x_w[j, :, i] = get_pd_pw(x_cls.get(j), i, x_features[:, i])[0]
+
+        t_s = prob_x_w.shape
+        n_prob_w_x = np.zeros((n_classes, t_s[1], t_s[2]))
+        for i in range(n_classes):
+            n_prob_w_x[i, :, :] = prob_x_w[i] * prob_w[i]
+        prob_x = np.sum(n_prob_w_x, axis=0)
+        # prob_w_x = np.zeros((n_classes, prob_x.shape[0], prob_w.shape[1]))
+        for i in range(n_classes):
+            prob_w_x[i, :, :] = n_prob_w_x[i, :, :]/prob_x
+
+        for i in range(x_features.shape[1]):
+            for j in range(n_trials):
+                t_sum = 0.0
+                for k in range(n_classes):
+                    if prob_w_x[k, j, i] > 0:
+                        t_sum += (prob_w_x[k, j, i] * np.log2(prob_w_x[k, j, i]))
+
+                h_w_x[i] -= (t_sum / n_trials)
+
+            mutual_info[i] = h_w - h_w_x[i]
+
+        mifsg = np.asarray(mutual_info)
+        return mifsg
+
+
+    def select_CSP_pairs(self,features_selected,n_pairs):
+        features_selected+=1
+        sel_groups = np.unique(np.ceil(features_selected/n_pairs))
+        paired_features = []
+        for i in range(len(sel_groups)):
+            for j in range(n_pairs-1,-1,-1):
+                paired_features.append(sel_groups[i]*n_pairs-j)
+
+        paired_features = np.asarray(paired_features,dtype=np.int)-1
+        print(paired_features)
+        return paired_features
